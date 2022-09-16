e0bfabd38608abdac99250d51b309067511c482c
emidde <marcosdaniel908@gmail.com>
2022-09-16 02:50:38 -0300

    atualização do blog

--- src/data.js
+++ src/data.js
@@ -1,54 +1,66 @@
 export const blogList = [
     {
         id: 1,
-        title: 'Como é viver em Porto?',
-        category: 'Portugal',
-        subCategory: ['Europa','Porto','Portugal'],
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        title: 'Mercado de t.i em Portugal',
+        category: 'Tecnologia',
+        subCategory: ['Europa','T.I','Portugal'],
+        description:[
+          <h1 className="mt-12 font-bold text-2xl mb-4">Como é trabalhar com T.I em portugal?</h1>,
+          'Em geral, toda a área de TI apresenta ofertas de trabalho em Portugal. Portanto, se você é um profissional com experiência comprovada em Tecnologia da Informação, há boas chances de encontrar uma oportunidade de trabalho no país.',
+          <br></br>,
+          <br></br>,
+          'Ao fazer uma rápida busca no site da EVPConnect, encontramos diversas oportunidades de trabalho disponíveis. Alguns cargos com posição em aberto são desenvolvedor Full stack, consultor de testadores de automação, desenvolvedor JAVA, desenvolvedor ETL, consultor QA/tester e especialista Front-end.',
+          <br></br>,
+          <br></br>,
+          'A julgar pelas estimativas para o mercado, as oportunidades na área de TI só tendem a aumentar. Conforme o site business.IT, até 2023 o mercado da Tecnologia da Informação deve crescer 33% em Portugal.',
+          <br></br>,
+          <br></br>,
+
+          'Com isso, é esperado que o número de trabalhadores em TI no país chegue a 160 mil pessoas, um aumento de 40 mil face aos números atuais.',
+          <h1 className="mt-12 font-bold text-2xl mb-4">Vagas nas áreas mais valorizadas no mercado de T.I</h1>,
+          <p>Veja agora exemplos de vagas de TI nas áreas mais valorizadas no mercado português. As oportunidades foram consultadas nos sites da EVPConnect, ITjobs e Expresso Emprego em maio de 2022.</p>,
+          <ul className="mt-8 block">
+            <ol>
+            <b>Desenvolvedor Mobile iOS:</b> formação em Engenharia Informática (ou similar), três anos de experiência em iOS, conhecimentos em desenvolvimento de aplicativos, arquitetura MVVM (-C), Objective-C, bibliotecas de terceiros e APIs, ciclo de vida do desenvolvimento móvel, APIs REST a partir de serviços de backend, Agile (Framework Scrum), comunicação em inglês, criatividade e capacidade de trabalho em equipe e resolução de problemas;
+            </ol>
+            <ol className="mt-6">
+            <b>Administrador de sistemas sênior:</b> mais de três anos de experiência com conhecimento em gerenciamento de sistemas em uma ou mais áreas de infra-estrutura (tecnologia Microsoft, Linux, VMWare, bancos de dados Oracle/SQL, sistemas centralizados de armazenamento e backup) e fluência em inglês;
+            </ol>
+            <ol className="mt-6">
+            <b>Administrador de sistemas e base de dados:</b> formação na área de TI, experiência de três anos, bons conhecimentos de inglês, certificações na área e domínio de ferramentas Linux e de soluções de virtualização;
+            </ol>
+            <ol className="mt-6">
+            <b>Técnico de Helpdesk:</b> formação em informática, um ano de experiência, bom conhecimento de inglês, carteira de habilitação, domínio de Microsoft, PHC e SQLserver e experiência com switching, routing e plataformas de virtualização;
+            </ol>
+            <ol className="mt-6">
+              <b>Engenheiro de DevOps:</b> mais de seis anos de experiências em DevOps, fluência em inglês, Experiência em microsserviços (Docker, Kubernetes) em ambiente de produção, habilidade de codificação em Python ou Ruby, forte conhecimento em Linux e linha de comando/scripts e princípios de gerenciamento de configuração e capacidade de projetar e implementar infraestrutura complexa;
+            </ol>
+            <ol className="mt-6">
+            <b>Programador Android:</b> formação em Engenharia Informática (ou similar), experiência de três anos em desenvolvimento Android, conhecimento em ambiente mobile, desenvolvimento de aplicativos e ciclo de vida do desenvolvimento e boa comunicação em inglês;
+            </ol>
+            <ol className="mt-6">
+            <b>Desenvolvedor de cloud:</b> experiência em construção em nuvens (AWS, Azure, GCP), infraestrutura e configuração como código, estratégias e implantação segura e CI/CD, DevOps e SRE, InfoSec e SIEM, experiência do desenvolvedor, além de boa capacidade de comunicação com clientes e equipe.
+            </ol>
+          </ul>,
+          <h1 className="mt-12 font-bold text-2xl mb-4">Vale a pena trabalhar na área de T.I em Portugal?</h1>,
+          <p className="mt-6">
+            Com certeza vale a pena trabalhar na área de TI em Portugal. Se você é um profissional com experiência e tem interesse em morar em Portugal, é uma ótima escolha.
+            <br></br>
+          <br></br>
+Como já foi comentado, esse setor é um dos que mais tem crescido no país e deve continuar em expansão nos próximos anos. Além disso, os salários normalmente ficam acima da média paga aos demais trabalhadores do país, deixando o custo de vida em Portugal bastante confortável.
+<br></br>
+          <br></br>
+Outra vantagem é que há muitas vagas em diferentes áreas da Tecnologia da Informação e os brasileiros são conhecidos em Portugal por serem excelentes profissionais do setor.
+<br></br>
+          <br></br>
+Aproveite e conheça a história do Luís que nos contou como é trabalhar em TI em Portugal.
+          </p>
+        
+        ],
         authorName: 'Mariza Estima',
-        authorAvatar: '/assets/images/cliente-1.png',
-        createdAt: 'Ago, 2022',
+        authorAvatar: '/assets/images/author.png',
+        createdAt: 'Setembro, 2022',
         cover:
           '/assets/images/porto.jpg',
-      },
-      {
-        id: 2,
-        title: 'Como é viver em Braga?',
-        category: 'Portugal',
-        subCategory: ['Europa','Braga','Portugal'],
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        authorName: 'Mariza Estima',
-        authorAvatar: '/assets/images/cliente-1.png',
-        createdAt: 'Ago, 2022',
-        cover:
-          '/assets/images/braga.jpg',
-      },
-      {
-        id: 3,
-        title: 'Como é viver em Lisboa?',
-        category: 'Portugal',
-        subCategory: ['Europa','Lisboa','Portugal'],
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        authorName: 'Mariza Estima',
-        authorAvatar: '/assets/images/cliente-1.png',
-        createdAt: 'Ago, 2022',
-        cover:
-          '/assets/images/lisboa.jpg',
-      },
-      {
-        id: 4,
-        title: 'Desenvolvedores em Portugal',
-        category: 'Portugal',
-        subCategory: ['Europa','Porto','Portugal', 'Lisboa', 'Braga', 'Dev'],
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        authorName: 'Mariza Estima',
-        authorAvatar: '/assets/images/cliente-1.png',
-        createdAt: 'Ago, 2022',
-        cover:
-          '/assets/images/dev.jpeg',
       }
 ];
